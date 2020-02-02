bb1cf92b85ea12832e07d276e537bc85cf877d20
Jenya <zhenyamaltseva@ya.ru>
2020-02-02 22:13:25 +0300

    pixelperfect about

--- src/blocks/swiper/swiper.js
+++ src/blocks/swiper/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'swiper';
 
-var mySwiper = new Swiper ('.swiper-container', {
+const mySwiper = new Swiper ('.swiper__container', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
@@ -9,8 +9,12 @@ var mySwiper = new Swiper ('.swiper-container', {
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper__pagination',
     clickable: true,
-    
+  },
+
+  navigation: {
+    nextEl: '.swiper__button_next',
+    prevEl: '.swiper__button_prev',
   },
 })
