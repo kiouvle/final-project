af64b73e86917df8e2a0f653ee232b9815b92ee8
Jenya <zhenyamaltseva@ya.ru>
2020-02-16 16:23:15 +0300

    showMore made private

--- src/js/components/NewsCardList.js
+++ src/js/components/NewsCardList.js
@@ -4,16 +4,16 @@ import HiddenElement from './HiddenElement';
 export default class NewsCardList extends HiddenElement {
   constructor(container) {
     super(container, 'result_hidden');
-    
+
     this._container = container;
     this._cardsContainer = this._container.querySelector('.result__card-container');
     this._newsButton = this._container.querySelector('.result__button');
     this._newsButton.addEventListener('click', () => {
-      this.showMore();
+      this._showMore();
     })
   }
 
-  showMore() {
+  _showMore() {
     this._cardsContainer.insertAdjacentHTML('beforeend', this._cardsArray.splice(0, 3).map((card) => this.addCard(card)).join(''));
     if (this._cardsArray.length === 0) {
       this._newsButton.classList.add('result__button_hidden');
@@ -25,7 +25,7 @@ export default class NewsCardList extends HiddenElement {
   render(cardsArray) {
     this._cardsArray = cardsArray;
     this._cardsContainer.innerHTML = '';
-    this.showMore();
+    this._showMore();
 
   }
 
